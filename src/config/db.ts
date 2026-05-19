f5bcc60e7a788b10ff91d095bf6aa7a528fa4440
Srijan Das <srijandas2018@gmail.com>
2026-05-19 13:03:30 +0530

    add sequelize sync

--- src/config/db.ts
+++ src/config/db.ts
@@ -31,7 +31,11 @@ export const connectDB = async () => {
   try {
     await sequelize.authenticate();
     console.log("Database connected ✅");
+
+    await sequelize.sync({ alter: true });
+    console.log("All tables synced ✅");
   } catch (error: any) {
     console.error("DB Error ❌:", error);
+    process.exit(1);
   }
 };
