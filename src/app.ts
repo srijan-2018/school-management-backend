d8124bb589c2efa85cbc704c983be3286a66e579
Srijan Das <srijandas2018@gmail.com>
2026-05-19 11:12:54 +0530

    fix render deployment

--- src/app.ts
+++ src/app.ts
@@ -12,29 +12,7 @@ import swaggerSpec from "./config/swagger";
 // ===============================
 // Import ALL models
 // ===============================
-import "./models/user.model";
-import "./models/class.model";
-import "./models/student.model";
-import "./models/subject.model";
-import "./models/role.model";
-import "./models/permission.model";
-import "./models/role-permission.model";
-import "./models/school.model";
-import "./models/section.model";
-import "./models/teacher.model";
-import "./models/parent.model";
-import "./models/parent-student.model";
-import "./models/teacher-class.model";
-import "./models/attendance.model";
-import "./models/exam.model";
-import "./models/mark.model";
-import "./models/student-document.model";
-import "./models/assignment.model";
-import "./models/assignment-submission.model";
-import "./models/timetable.model";
-import "./models/fee.model";
-import "./models/fee-payment.model";
-import "./models/mock-test.model";
+import "./models";
 
 // ===============================
 // Routes
